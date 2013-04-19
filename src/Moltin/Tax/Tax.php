0458f8385eeecfdb5d56debe524ec2a2839e56f2
Chris Harvey <chris@chrisnharvey.com>
2013-04-19 16:20:03 +0100

    Assign the percentage to a class variable

--- src/Moltin/Tax/Tax.php
+++ src/Moltin/Tax/Tax.php
@@ -2,10 +2,12 @@
 
 class Tax
 {
+    protected $percentage;
     protected $taxModifier;
     
     public function __construct($value)
     {
+        $this->percentage = $value;
         $this->taxModifier = 1 - ($value / 100);
     }
 }
