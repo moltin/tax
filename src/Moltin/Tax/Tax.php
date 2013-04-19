94f4fa2c7f48d2f1293f34d2c878a6c50ee14c75
Chris Harvey <chris@chrisnharvey.com>
2013-04-19 16:21:12 +0100

    Adding "deduct" method to deduct the tax from a specified value

--- src/Moltin/Tax/Tax.php
+++ src/Moltin/Tax/Tax.php
@@ -10,4 +10,9 @@ class Tax
         $this->percentage = $value;
         $this->taxModifier = 1 - ($value / 100);
     }
+
+    public function deduct($price)
+    {
+        return $price * $this->taxModifier;
+    }
 }
