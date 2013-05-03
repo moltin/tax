b25ac9b4718db7d6926c2887c6dc3a9410a8c4a2
Chris Harvey <chris@chrisnharvey.com>
2013-05-03 10:59:04 +0100

    Adding copyright

--- src/Moltin/Tax/Tax.php
+++ src/Moltin/Tax/Tax.php
@@ -1,4 +1,24 @@
-<?php namespace Moltin\Tax;
+<?php
+
+/**
+ * This file is part of Moltin Tax, a PHP package to calculate
+ * tax rates.
+ *
+ * Copyright (c) 2013 Moltin Ltd.
+ * http://github.com/moltin/tax
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package moltin/tax
+ * @author Chris Harvey <chris@molt.in>
+ * @copyright 2013 Moltin Ltd.
+ * @version dev
+ * @link http://github.com/moltin/tax
+ *
+ */
+
+namespace Moltin\Tax;
 
 class Tax
 {
--- tests/TaxTest.php
+++ tests/TaxTest.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * This file is part of Moltin Tax, a PHP package to calculate
+ * tax rates.
+ *
+ * Copyright (c) 2013 Moltin Ltd.
+ * http://github.com/moltin/tax
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package moltin/tax
+ * @author Chris Harvey <chris@molt.in>
+ * @copyright 2013 Moltin Ltd.
+ * @version dev
+ * @link http://github.com/moltin/tax
+ *
+ */
+
 use Moltin\Tax\Tax;
 
 class TaxTest extends \PHPUnit_Framework_TestCase
